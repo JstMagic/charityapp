2ab97e1b7a7f28d713dadba561c2050271c0a35c
nkdroid007 <contact@nkdroidsolutions.com>
2019-02-20 19:22:01 +0530

    asasasaassas

---
+++ app/src/main/java/com/kodemakers/charity/model/StaffResult.java
@@ -0,0 +1,31 @@
+
+package com.kodemakers.charity.model;
+
+import java.util.ArrayList;
+
+import com.google.gson.annotations.SerializedName;
+
+
+public class StaffResult {
+
+    @SerializedName("result")
+    private ArrayList<StaffDetails> mResult;
+    @SerializedName("status")
+    private Long mStatus;
+
+    public ArrayList<StaffDetails> getResult() {
+        return mResult;
+    }
+
+    public void setResult(ArrayList<StaffDetails> result) {
+        mResult = result;
+    }
+
+    public Long getStatus() {
+        return mStatus;
+    }
+
+    public void setStatus(Long status) {
+        mStatus = status;
+    }
+}
